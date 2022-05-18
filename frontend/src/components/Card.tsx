3e25a4f4d36fb8a86f18605756f8448633be194d
Wataru Okada <wataru.okada@flect.co.jp>
2022-05-18 19:41:23 +0900

    update

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -7,9 +7,9 @@ export const Card = () => {
             <div className="m-4">
                 <h1 className="text-xl ">Screen Capture with ffmpeg-wasm</h1>
                 <h2 className="text-base underline font-black pt-4">Usage</h2>
-                <p className="m-1">(1) push load file button and select movie file</p>
-                <p className="m-1">(2) select crop area from movie. set start time and end time by using time slide bar.</p>
-                <p className="m-1">(3) push convert button.</p>
+                <p className="m-1">(1) push choose window button and select windows</p>
+                <p className="m-1">(2) push start rec. button. </p>
+                <p className="m-1">(3) And then, when you want to stop rec, push the button.</p>
 
                 <h2 className="text-base underline font-black pt-4">Please support me</h2>
                 <p className="m-1">You can support coffee here.</p>
