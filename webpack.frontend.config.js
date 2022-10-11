2dc95e8301e473cbd0d125a79758a59d32e11872
Wataru Okada <wataru.okada@flect.co.jp>
2022-10-11 11:16:14 +0900

    update

--- webpack.frontend.config.js
+++ webpack.frontend.config.js
@@ -2,6 +2,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+
 module.exports = {
     // mode: "development",
     mode: "production",
