1f7e9ebf510b1095ee3e5d87d880398b27d81f24
Wataru Okada <wataru.okada@flect.co.jp>
2022-11-15 09:20:49 +0900

    update

--- frontend/src/100_components/002_parts/330_Links.tsx
+++ frontend/src/100_components/002_parts/330_Links.tsx
@@ -4,7 +4,7 @@ export const Links = () => {
 
     return (
         <div className="sidebar-content">
-            <div className="sidebar-content-row-3-7">
+            <div className="sidebar-content-row-5-5">
                 <div className="sidebar-content-row-label"><a href="https://w-okada.github.io/ffmpeg-cli-gen-js/">ffmpeg-cli-gen-js</a></div>
                 <div className="sidebar-content-row-label">Simple Movie Editor</div>
 
